60ef709c7390d451a2a396d7318bff7bf8960d7a
Rafaella Matos <prof.rafaella.matos@gmail.com>
2023-10-01 15:15:11 -0300

    rota para a página inicial

---
+++ app.js
@@ -0,0 +1,23 @@
+// app.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const db = require('./src/dbConfig'); // Importe o arquivo de configuração
+
+const app = express();
+const port = 3000;
+
+// Configurar middleware para servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
+// Rotas
+// Rota para a página inicial
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Inicie o servidor
+app.listen(port, () => {
+    console.log(`Servidor iniciado na porta ${port}`);
+});
